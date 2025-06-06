fcba773f81a65c2b64ec706a0ca7a67111830c70
Evgenii Grebennikov <djonnyx@gmail.com>
2025-06-06 17:56:22 +0300

    Improved compression

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  *  Качество сжатия ~ в 2 раза от исходной разделенной сепараторами последовательности чисел
  */
 
-const SEPARATOR = '-';
+const SEPARATOR = '-', W_PATTERN = /([a-zA-Z])/g;
 
 /**
  * Сереализация чиселового массива
@@ -17,7 +17,13 @@ const serialize = (arr, separator = SEPARATOR) => {
         const s = String(Number(e).toString(32));
         result.push(s);
         if (s.length < 2) {
-            result.push(separator);
+            const lastChar = s.charAt(s.length - 1);
+            if (W_PATTERN.test(lastChar)) {
+                const v = s.substring(0, s.length - 2);
+                result[result.length - 1] = `${v}${s.charAt(s.length - 1).toUpperCase()}`;
+            } else {
+                result.push(separator);
+            }
         }
     });
     return result.join('');
@@ -33,12 +39,12 @@ const deserialize = (src, separator = SEPARATOR) => {
     const result = [];
     let value = '';
     for (let i = 0, l = src.length; i < l; i++) {
-        const char = src[i], isSep = char === separator;
+        const char = src[i], isSep = char === separator, isLastCharAsSep = W_PATTERN.test(char) && char === char.toUpperCase();
         if (!isSep) {
             value += char;
         }
-        if ((isSep && value.length === 1) || value.length === 2) {
-            result.push(Number.parseInt(value, 32));
+        if ((isSep && value.length === 1) || value.length === 2 || isLastCharAsSep) {
+            result.push(Number.parseInt(value.toLowerCase(), 32));
             value = '';
         }
     }
