13d1893221eb03dc72ddfd4b8e756ca9c0b16f81
Evgenii Grebennikov <djonnyx@gmail.com>
2025-06-07 10:24:22 +0300

    compression options added

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const SEPARATOR = '-', W_PATTERN = /([a-zA-Z])/;
 
 /**
+ * @typedef {object} ISerializeOptions
+ * @property {string} separator - A string character. An optional object property with a default value ("-")
+ * @property {boolean} caseSensetive - If true, then capital letters will be used as separators whenever possible. An optional object property with a default value (true)
+ */
+const DEFAULT_SERIALIZE_OPTIONS = {
+    separator: SEPARATOR,
+    caseSensetive: true,
+};
+
+/**
  * Serialization of a numeric array
  * @param {Array<number>} arr 
- * @param {string} separator optional
+ * @param {ISerializeOptions} options
  * @returns 
  * @author djonnyx@gmail.com
  * Numeric array compressor
@@ -13,18 +23,22 @@ const SEPARATOR = '-', W_PATTERN = /([a-zA-Z])/;
  * const compressed = serialize(arr); 
  * console.log(compressed); // "0-34P7-1fkq"
  */
-const serialize = (arr, separator = SEPARATOR) => {
-    const result = [];
+const serialize = (arr, options = DEFAULT_SERIALIZE_OPTIONS) => {
+    const opt = { ...DEFAULT_SERIALIZE_OPTIONS, ...options ?? {} }, result = [];
     arr.forEach(e => {
         const s = String(Number(e).toString(32));
         result.push(s);
         if (s.length < 2) {
-            const lastChar = s.charAt(s.length - 1);
-            if (W_PATTERN.test(lastChar)) {
-                const v = s.substring(0, s.length - 2);
-                result[result.length - 1] = `${v}${s.charAt(s.length - 1).toUpperCase()}`;
+            if (opt.caseSensetive) {
+                const lastChar = s.charAt(s.length - 1);
+                if (W_PATTERN.test(lastChar)) {
+                    const v = s.substring(0, s.length - 2);
+                    result[result.length - 1] = `${v}${s.charAt(s.length - 1).toUpperCase()}`;
+                } else {
+                    result.push(opt.separator);
+                }
             } else {
-                result.push(separator);
+                result.push(opt.separator);
             }
         }
     });
@@ -34,18 +48,19 @@ const serialize = (arr, separator = SEPARATOR) => {
 /**
  * Deserializing a Numeric Array
  * @param {string} src 
- * @param {string} separator optional
+ * @param {ISerializeOptions} options
  * @returns 
  * @example 
  * const compressed = "0-34P7-1fkq";
  * const arr: Array<number> = deserialize(compressed); 
  * console.log(arr); // "[0, 100, 25, 7, 47, 666]"
  */
-const deserialize = (src, separator = SEPARATOR) => {
-    const result = [];
+const deserialize = (src, options = DEFAULT_SERIALIZE_OPTIONS) => {
+    const opt = { ...DEFAULT_SERIALIZE_OPTIONS, ...options ?? {} }, result = [];
     let value = '';
     for (let i = 0, l = src.length; i < l; i++) {
-        const char = src[i], isSep = char === separator, isLastCharAsSep = W_PATTERN.test(char) && char === char.toUpperCase();
+        const char = src[i], isSep = char === opt.separator,
+            isLastCharAsSep = opt.caseSensetive && W_PATTERN.test(char) && char === char.toUpperCase();
         if (!isSep) {
             value += char;
         }
--- spec.js
+++ spec.js
@@ -19,22 +19,22 @@ const specGenerateListsForBenchmark = (iterations = BENCHMARK_ITERATIONS) => {
     return result;
 }
 
-const mesureCompression = (lists) => {
+const mesureCompression = (lists, withCapitalLatters = false) => {
     const iterations = lists.length;
     let summ = 0;
     for (let i = 0, l = iterations; i < l; i++) {
         const list = lists[i];
-        summ += JSON.stringify(list).length / serialize(list).length
+        summ += JSON.stringify(list).length / serialize(list, { caseSensetive: withCapitalLatters }).length
     }
-    return summ / iterations;
+    return `${Number(summ / iterations * 100).toFixed(2)} %`;
 }
 
-const deserializeMustBeCorrect = (lists) => {
+const deserializeMustBeCorrect = (lists, withCapitalLatters = false) => {
     const iterations = lists.length;
     let isCorrect = false;
     for (let i = 0, l = iterations; i < l; i++) {
         const list = lists[i];
-        isCorrect = JSON.stringify(deserialize(serialize(list))) === JSON.stringify(list);
+        isCorrect = JSON.stringify(deserialize(serialize(list, { caseSensetive: withCapitalLatters }), { caseSensetive: withCapitalLatters })) === JSON.stringify(list);
         if (!isCorrect) {
             return isCorrect;
         }
@@ -44,6 +44,18 @@ const deserializeMustBeCorrect = (lists) => {
 
 const LISTS = specGenerateListsForBenchmark();
 
+const isCorrect = (v, msg) => {
+    if (v) {
+        return `\x1b[32m ${msg} \x1b[0m`;
+    }
+
+    return `\x1b[41m ${msg} \x1b[0m`;
+}
+
 console.info(`Benchmark on ${LISTS.length} elements:`)
-console.log(` - compression: ${mesureCompression(LISTS)}`);
-console.log(` - deserialize must be correct: ${deserializeMustBeCorrect(LISTS)}`);
+console.log(` - compression (With capital letters): \x1b[34m ${mesureCompression(LISTS, true)} \x1b[0m`);
+console.log(` - compression (Lowercase letters only): \x1b[34m ${mesureCompression(LISTS, false)} \x1b[0m`);
+const isCorrect1 = deserializeMustBeCorrect(LISTS, true);
+console.log(isCorrect(isCorrect1, `- deserialize must be correct (With capital letters): ${isCorrect1}`));
+const isCorrect2 = deserializeMustBeCorrect(LISTS, false);
+console.log(isCorrect(isCorrect2, `- deserialize must be correct (Lowercase letters only): ${isCorrect2}`));
